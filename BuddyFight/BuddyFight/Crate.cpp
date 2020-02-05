9cb03bf74f1d050df45dc1ce580290b933e6fc7d
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2020-02-05 16:52:32 -0500

    Deleted Old Script/Crate.cpp
    Made the Crate.cpp, deleted old levelPlatform script thats not needed now.

---
+++ BuddyFight/BuddyFight/Crate.cpp
@@ -0,0 +1,10 @@
+#include "Crate.h"
+
+Crate::Crate(float x , float y) : 
+	Platform(new Texture("TestLevelSpriteSheet.png",64,64,64,64),x,y)
+{
+}
+
+Crate::~Crate()
+{
+}
---
+++ BuddyFight/BuddyFight/Crate.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Platform.h"
+#include "EntityPool.h"
+class Crate : public Platform
+{
+	private:
+
+	protected:
+
+	public:
+
+
+		Crate(float x, float y);
+		~Crate();
+
+};
+
