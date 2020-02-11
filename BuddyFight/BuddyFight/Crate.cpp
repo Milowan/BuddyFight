a3fcfa76b123a9552c30a53b1afe63ad045dd1ab
Milowan <44212243+Milowan@users.noreply.github.com>
2020-02-10 21:26:18 -0500

    fixed crate sizing

--- BuddyFight/BuddyFight/Crate.cpp
+++ BuddyFight/BuddyFight/Crate.cpp
@@ -2,7 +2,10 @@
 
 Crate::Crate(float x , float y) : 
 	Platform(new Texture("TestLevelSpriteSheet.png",64,64,64,64),x,y)
-{}
+{
+	texture->SetHeight(Graphics::BLOCK_HEIGHT * 2);
+	texture->SetWidth(Graphics::BLOCK_WIDTH * 2);
+}
 
 Crate::~Crate()
 {}
